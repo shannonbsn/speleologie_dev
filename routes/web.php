b83d7770f040cf549d6c4f1b7c752c9936cc0a35
Shannon Besson <shannon0413@outlook.com>
2024-04-19 11:42:55 +0200

    apparition formulaire dans l'espace moderateur

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeformController;
+use App\Http\Controllers\ModerateurController;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard',[ModerateurController::class, 'showModerateur'])->name('dashboard');
+
 });
 
 Route::get('/experience',[HomeformController::class, 'create'])->name('experience');
